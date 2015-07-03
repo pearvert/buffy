6033836538843e3e38bd76deb217b4e410e34cb6
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-07-02 21:03:46 -0400

    implementation of XLSX_to_JSON_API
    run npm start and then visit localhost:3000/

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
+var xlsx_api = require('xls_to_json_api');
 var express = require('express');
 var serveIndex = require('serve-index');
 var app = express();
 
 app.use(express.static('./'));
-app.use('/', serveIndex('./'));
 
 app.get('/express', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var sheets = [
+  "andrew",
+  "angel",
+  "anya",
+  "buffy",
+  "cordelia",
+  "dawn",
+  "demons",
+  "drusilla",
+  "faith",
+  "giles",
+  "human",
+  "jonathan",
+  "Oz",
+  "riley",
+  "spike",
+  "vampires",
+  "willow",
+  "xander",
+  "potentials",
+  "allSlayers",
+  "totals"
+];
 
+xlsx_api(app, {
+  sheets: sheets,
+  xlsxSource: 'deathDatasets.xlsx',
+  dataDir: __dirname+'/data'
+});
+
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('Service listening at http://%s:%s', host, port);
 });
